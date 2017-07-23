3166c3bf149927de5f104de56cb16c0051fcfad6
TeeNik <teenik3315@gmail.com>
2017-07-23 21:44:40 +0300

    first

---
+++ Assets/_Scripts/WeaponController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponController : MonoBehaviour {
+
+    GameObject bullet;
+    float fireTime = .5f;
+    int poolAmount = 1000;
+    List<GameObject> bullets;
+
+	// Use this for initialization
+	void Start () {
+        bullets = new List<GameObject>();
+        for(int i = 0; i < poolAmount; ++i)
+        {
+            GameObject g = (GameObject)Instantiate(bullet);
+            g.SetActive(false);
+            bullets.Add(g);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
